98f063e51aa479845a9e2effbe656fb1df6a2fa0
todoi <steelangeltodoi@gmail.com>
2017-12-01 11:21:33 +0800

    add open folder

--- pages/js/rolldao.js
+++ pages/js/rolldao.js
@@ -1,3 +1,4 @@
+
 ;(function($){
 	$.fn.extend({        
 		rollOdao:function(o){ 
@@ -5,28 +6,29 @@
 				speed: 50,   //滚动速度
 				dir: "top",
 				width:0				
-			};
+            };
+
 			o = $.extend(defaults, o);      
 			return this.each(function(){
-				var _speed = o.speed;
-				var _rollchild = $(this).children();
-				var _rollele = _rollchild.parent();
-				var _rollheight = _rollchild.height(); 
-				var _rollwidth = 0;
+				var _speed = o.speed; 
+				var _rollchild = $(this).children(); //ul  
+				var _rollele = _rollchild.parent(); //div
+				var _rollheight = _rollchild.height(); //ul 的高度
+				var _rollwidth = 0; //原始的宽度为0
 				if( o.width != 0){
 					_rollchild.width(o.width*2);
 					_rollwidth =  o.width;
 				}else{
-					_rollwidth=_rollchild.width();
+					_rollwidth=_rollchild.width(); // ul的宽度
 				}
-				if(_rollheight>_rollele.height()){
+				if(_rollheight>_rollele.height()){ //ul 的高度 大于 div 的高度
 					_rollchild.html(_rollchild.html()+_rollchild.html());
 				}
 				
 				
 				function rollgo(){ 					
 					if(o.dir == 'top'){
-						if(_rollele.scrollTop() >= _rollheight){
+						if(_rollele.scrollTop() >= _rollheight){ 
 							_rollele.scrollTop(0); 
 						}else{
 							 _rollele.scrollTop(_rollele.scrollTop()+1); 
@@ -51,3 +53,6 @@
 		}       
 	});  
 })(jQuery); 
+
+//会有一个bug,因为 ul设置的宽度为 li.width()*3*2 = 1680, div的宽度为 1072; 所以scroll 的宽度只为608 宽度
+//只要将ul 的宽度是div 的2倍多。
