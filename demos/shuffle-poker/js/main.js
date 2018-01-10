c12afa5175715c72d04b1d81148e44d0b9882fc6
todoi <steelangeltodoi@gmail.com>
2018-01-10 11:02:57 +0800

    init shuffle-poker jqury plugin precondition

---
+++ demos/shuffle-poker/js/main.js
@@ -0,0 +1,18 @@
+function start(){
+  console.log(1)
+}
+
+
+start()
+
+
+function want(){
+  console.log(2)
+  console.log(2)
+  console.log(2)
+  console.log(2)
+  console.log(2)
+  console.log(2)
+}
+
+want()
