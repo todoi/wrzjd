74c7ee1787c2dd57ebd972fbc227407020b6fa9e
todoi <steelangeltodoi@gmail.com>
2018-01-12 19:20:30 +0800

    try gulp and sass

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')
+
+gulp.task('default', function() {
+  // 将你的默认的任务代码放在这
+});
+
+gulp.task('watch', function(){
+    return gulp.watch('js/**/*.js', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('sass', [], ()=>{
+    return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('sass:watch', ()=>{
+    gulp.watch('./sass/**/*.scss', ['sass'])
+})
+
+
